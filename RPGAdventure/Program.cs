b08e4e52d7609264375ce732354cd0465ffedac9
Imagineer-Ty Github <tyrone.brown5311@gmail.com>
2024-02-04 23:28:40 -0500

    new classes/methods for starting game

--- RPGAdventure/Program.cs
+++ RPGAdventure/Program.cs
@@ -2,17 +2,39 @@
 
 namespace AdventureRPG {
 
+    public static class Game {
+
+        //name of character
+        static string CharacterName = "Player 1";
+
+        //print game title
+        public static void StartGame()
+        {
+            Console.WriteLine("Dungeon Crawler");
+            Console.WriteLine("Hello, you seem like you are ready for an adventure.");
+            NameCharacter();
+        }
+        //player name input 
+        static void NameCharacter()
+        {
+            Console.WriteLine("What's your name? ");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Hmm... " + CharacterName + "... What a strange name.");
+        }
+        static void Dialog(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+
+    class Item { }
+
     class Program {
         static void Main()
         {
-            string CharacterName = "Player 1";
-
-            Console.WriteLine("Dungeon Crawler");
-            Console.WriteLine("Hello, you seem like you are ready for an adventure.");
-            Console.WriteLine("What's your name? ");
-            CharacterName = Console.ReadLine();
-            Console.WriteLine("Hmm... " + CharacterName + "... What a strange name." );
-
+            Game.StartGame();
             Console.ReadKey();
         }
             }
