17bb63b86d80a24bac98261e829bd062b72821c7
Imagineer-Ty Github <tyrone.brown5311@gmail.com>
2024-02-04 22:18:56 -0500

    Add project files.

---
+++ RPGAdventure/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace AdventureRPG {
+
+    class Program {
+        static void Main()
+        {
+            string CharacterName = "Player 1";
+
+            Console.WriteLine("Dungeon Crawler");
+            Console.WriteLine("Hello, you seem like you are ready for an adventure.");
+            Console.WriteLine("What's your name? ");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Hmm... " + CharacterName + "... What a strange name." );
+
+            Console.ReadKey();
+        }
+            }
+    }
